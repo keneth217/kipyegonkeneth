a3d76faeb789d72ecada943061257e4c603df325
keneth217 <kipyegonkeneth03@gmail.com>
2023-10-10 12:44:48 +0300

    added test view

---
+++ src/views/TestView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="background" >
+        hhhh
+    </div>
+</template>
+<style scoped>
+.background{
+    background-size: cover; 
+    background-image: url(./../assets/strait.svg);
+    width: 100%;
+    height: 100%;
+    display: block;
+    background-position: center;
+    background-repeat: no-repeat
+    ;
+
+  }
+  .title {
+    margin-top: 20px;
+    font-size: 30px;
+    color: #a31181;
+    box-shadow: 5px ;
+    font-family: "Times New Roman", Times, serif;
+  }
+</style>
