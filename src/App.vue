b717582933fdacd8c033d8b39cf8ddbe628c3e31
keneth217 <kipyegonkeneth03@gmail.com>
2023-10-12 17:37:24 +0300

    appp update

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <v-app>
+   
+  
     <v-main>
       <NavigationBar />
-      <FooterComponent/>
-     
-   <router-view v-slot="{component}">
-     <transition name="router" mode="out-in">
-       <component :is="component"></component>
-   </transition>
-   </router-view>
+   
+    <!--       -->
+    <!-- <router-view v-slot="{component}"> -->
+    <!-- <transition name="router" mode="out-in"> -->
+    <!-- <component :is="component"></component> -->
+    <!-- </transition> -->
+    <!-- </router-view> -->
+    <router-view></router-view>
+    <FooterComponent />
     </v-main>
   </v-app>
 </template>
 
 <script>
 import NavigationBar from "@/components/NavigationBar.vue";
-import FooterComponent from "@/components/FooterComponent.vue"
+import FooterComponent from "@/components/FooterComponent.vue";
 //import FooterView from "@/views/FooterView.vue"
 export default {
   name: "App",
@@ -26,18 +30,24 @@ export default {
 };
 </script>
 <style>
-  .router-enter-from{
-    opacity:0;
-    transform:translateX(100px);
-  }
-    .router-enter-active{
-   transition:all 0.3s ease-out;
-  }
-    .router-enter-to{
-    opacity:0;
-    transform:translateX(-100px);
-  }
-    .router-leave-active{
-   transition:all 0.3s ease-in;
-  }
-  </style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'montserrat','sans-serif';
+}
+.router-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.router-enter-active {
+  transition: all 0.3s ease-out;
+}
+.router-enter-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.router-leave-active {
+  transition: all 0.3s ease-in;
+}
+</style>
